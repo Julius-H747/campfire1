033255c8dfe0e9422884969a64e437500cc7ca07
rohan flynn <rohanjflynn@gmail.com>
2026-03-01 17:41:30 +1000

    final

--- campfire/area_2d.gd
+++ campfire/area_2d.gd
@@ -2,18 +2,30 @@ extends Area2D
 
 @onready var animated_door = $"../Area2D/AnimatedSprite2D"
 var on = false
+var p1 = false
+var p2 = false
 
 func _on_body_entered(body: CharacterBody2D) -> void:
 	if body.is_in_group("player"):
 		on = true
+		p1 = true
+	if body.is_in_group("player2"):
+		p2 = true
 
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		on = false
+		p1 = false
+	if body.is_in_group("player2"):
+		p2 = false
 
+		
+		
 func _physics_process(delta: float) -> void:
 	if on:
 		animated_door.play("door")
 	else:
 		animated_door.play("no")
+	if p1 and p2:
+		get_tree().change_scene_to_file("res://ufo.tscn")

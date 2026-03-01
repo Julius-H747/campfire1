738aa491cc3d017ac6d72d0d18a3e9b968758490
rohan flynn <rohanjflynn@gmail.com>
2026-03-01 11:46:26 +1000

    finally done level1

--- campfire/end_level1.gd
+++ campfire/end_level1.gd
@@ -11,16 +11,21 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if p1 and p2:
-		get_tree().change_scene_to_file("res://cutscene1.tscn")
+		get_tree().change_scene_to_file("res://level3.tscn")
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		print("p1")
 		p1 = true
-	else:
+	if body.is_in_group("player2"):
+		print(p2)
+		p2 = true
+		
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
 		p1 = false
 	if body.is_in_group("player2"):
-		p2 = true
-	else:
 		p2 = false
-		

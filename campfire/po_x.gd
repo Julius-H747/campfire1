4f3912014699d14ec3fcd79037c24d2dea7b9674
rohan flynn <rohanjflynn@gmail.com>
2026-03-01 16:07:17 +1000

    stuff

--- campfire/po_x.gd
+++ campfire/po_x.gd
@@ -1,16 +1,17 @@
 extends TileMapLayer
 
-
+var og
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	visible = true
-	collision_enabled = true
+	og = position.x
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func on():
-	visible = false
-	collision_enabled = false
+func push(p):
+	global_position.x += p
+
+func reset():
+	global_position.x = og

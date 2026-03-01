e2432c875e8e09081ea40c71bcd49e72c27f3ab0
Julius-H747 <julius.hold@gmail.com>
2026-03-01 12:54:23 +1000

    door

--- campfire/area_2d.gd
+++ campfire/area_2d.gd
@@ -1,6 +1,19 @@
 extends Area2D
 
 @onready var animated_door = $"../Area2D/AnimatedSprite2D"
+var on = false
 
 func _on_body_entered(body: CharacterBody2D) -> void:
-	animated_door.play("door")
+	if body.is_in_group("player"):
+		on = true
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		on = false
+
+func _physics_process(delta: float) -> void:
+	if on:
+		animated_door.play("door")
+	else:
+		animated_door.play("no")
